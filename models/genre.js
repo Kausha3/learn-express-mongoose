157fb8bd0489af432343b5998a1d4fb76a53e853
Kausha3 <133591404+Kausha3@users.noreply.github.com>
2024-03-14 15:23:54 -0400

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  { name: { type: String, required: true, minlength: 3, maxlength: 100 },
+  url: { type: String, required: true }}
 );
 
 
